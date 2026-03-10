9ae8833cbadc807f0fba4a2993fb2ce82d445d2d
alexandr.anashkevich <alexandr.anashkevich@compit.by>
2026-03-10 15:03:53 +0300

    AA fix baseURL

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://f1548a21ed601512-213-184-226-185.serveousercontent.com",
+  baseURL: "http://localhost:8080",
   headers: {
     "Content-Type": "application/json",
   },
