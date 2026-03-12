e93ebcc10190d32adcd1ce9365f606765c2838a1
alexandr.anashkevich <alexandr.anashkevich@compit.by>
2026-03-12 12:52:32 +0300

    AA fix security

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://10.0.2.41:8080",
   headers: {
     "Content-Type": "application/json",
   },
