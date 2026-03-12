fe98afdc3f7bb05ffb61691e7e39847601bd87fa
alexandr.anashkevich <alexandr.anashkevich@compit.by>
2026-03-12 12:17:20 +0300

    AA fix frontend

---
+++ frontend/src/components/forkliftTimeout/forkliftTimeoutData.ts
@@ -0,0 +1,20 @@
+export const forkliftTimeoutFields = [
+  {
+    name: "detectedDate",
+    label: "Начало",
+    type: "datetime-local",
+    placeholder: "",
+  },
+  {
+    name: "solutionDate",
+    label: "Окончание",
+    type: "datetime-local",
+    placeholder: "",
+  },
+  {
+    name: "description",
+    label: "Причина",
+    type: "text",
+    placeholder: "Введите причину",
+  },
+];
---
+++ frontend/src/pages/main/forkliftData.ts
@@ -0,0 +1,46 @@
+export const forkliftCreateFields = [
+  {
+    name: "brand",
+    label: "Марка",
+    type: "text",
+    placeholder: "Введите марку",
+  },
+  {
+    name: "number",
+    label: "Номер",
+    type: "text",
+    placeholder: "Введите номер",
+  },
+  {
+    name: "loadCapacity",
+    label: "Грузоподъемность",
+    type: "float",
+    placeholder: "Введите грузоподъемность",
+  },
+];
+export const forkliftUpdateFields = [
+  {
+    name: "isActive",
+    label: "Активный",
+    type: "checkbox",
+    placeholder: "Активный",
+  },
+  {
+    name: "brand",
+    label: "Марка",
+    type: "text",
+    placeholder: "Введите марку",
+  },
+  {
+    name: "number",
+    label: "Номер",
+    type: "text",
+    placeholder: "Введите номер",
+  },
+  {
+    name: "loadCapacity",
+    label: "Грузоподъемность",
+    type: "float",
+    placeholder: "Введите грузоподъемность",
+  },
+];
